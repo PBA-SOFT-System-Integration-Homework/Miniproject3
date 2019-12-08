a00a550fcd68f51cf45a7b0bceb430a3c8de92d3
Stanislav Novitski <Stanislav.Novitski@ibm.com>
2019-12-08 13:08:50 +0100

    more stuff works :)

--- rating-service/server.js
+++ rating-service/server.js
@@ -1,16 +1,64 @@
-const express = require('express')
-const app = express()
-const PORT = 3004
+const express = require('express');
+const app = express();
+const PORT = 3004;
+
+app.use(express.json());
 
 const ratingDB = []
 
-app.post('/', (req, res, next) => {
-    let feedback = JSON.parse(req.body.feedback)
-    ratingDB.push(feedback)
+
+app.get('/questions/rating', (req, res, next) => {
+    const ratingQuestions = [
+        {
+            type: 'list',
+            choices: ['*', '**', '***', '****', '*****',],
+            name: 'Rating',
+            message: "'How would you rate the quality of the service?'"
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: "'Please enter any feedback to your order / the quality of the service'"
+        },
+    ];
+
+    res.json(ratingQuestions);
+});
+
+app.get('/questions/additional', (req, res, next) => {
+    const questions = [
+
+        {
+            type: 'input',
+            name: 'Location',
+            message: "'What is your geographical location?'"
+        },
+        {
+            type: 'list',
+            choices: ['Man', 'Woman', 'Non-binary', 'Prefer not to disclose'],
+            name: 'Gender',
+            message: "'What is your gender?'"
+        },
+        {
+            type: 'list',
+            choices: ['18-24', '25-39', '40-60', '60 plus'],
+            name: 'Age',
+            message: "'What age range are you in?'"
+        }
+    ];
+
+    res.json(questions);
+})
+
+// POST Feedback 
+app.post('/feedback', (req, res, next) => {
+    console.log(req.body);
+    ratingDB.push(req.body);
     res.end("Feedback submitted")
 })
 
-app.get('/', (req, res, next) => {
+// GET all feedbacks
+app.get('/feedback', (req, res, next) => {
     let allFeedback = JSON.stringify(ratingDB)
     res.end(allFeedback)
 })
